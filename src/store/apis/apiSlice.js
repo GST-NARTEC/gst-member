2732a9188a8ba3e78a8ad970f227459b41b5f885
Sadeeq499 <muhammadsadeeq11223@gmail.com>
2024-11-25 18:42:19 +0500

    base url changed

--- src/store/apis/apiSlice.js
+++ src/store/apis/apiSlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://api.gstsa1.org/api",
-    baseUrl: "http://localhost:3000/api",
+    baseUrl: "https://api.gstsa1.org/api",
+    // baseUrl: "http://localhost:3000/api",
     // both url is above dev and testing
     // credentials: "include",
     // credentials: "include",
